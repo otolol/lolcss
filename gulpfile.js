5b32583b3ce16c8de54c6e513b89833a964f765c
oto <otololua@gmail.com>
2016-02-16 01:01:28 +0400

    gulpfile: add perfectionist and move autoprefixer from gulp to postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer')
+var autoprefixer = require('autoprefixer');
+var perfectionist = require('perfectionist');
+var postcss = require('gulp-postcss');
 
 gulp.task('sass', function(){
     gulp.src('src/app.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
+        .pipe(postcss([
+                perfectionist({
+                    format: 'expanded',
+                }),
+                autoprefixer()
+            ]
+        ))
         .pipe(gulp.dest('build/'))
 })
 
